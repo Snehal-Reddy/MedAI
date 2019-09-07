2122412e2a63b028032a788181723644269eed05
j-99 <jeevankur.iit@gmail.com>
2019-09-07 17:00:01 +0530

    ui updates and frontend

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("main.html")
 
 @app.route('/image', methods = ['POST'])
 def solve():
